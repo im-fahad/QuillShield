0bf701b62ea5ffd3490bc762e3b40126a4171cf2
im-fahad <abdullahalfahad.bd@gmail.com>
2024-04-09 06:31:38 +0600

    folder files menu added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,73 @@
 import Image from "next/image";
 import Icon from '@mdi/react';
-import {mdiChevronDown} from '@mdi/js';
+import {mdiChevronDown, mdiFile, mdiFolder, mdiFolderOutline, mdiMenuDown, mdiMenuRight} from '@mdi/js';
 
 export default function Home() {
+    const folderFiles = [
+        {
+            name: "Example Folder",
+            type: "directory",
+            isOpen: true,
+            children: [
+                {
+                    name: "Sample Folder",
+                    type: "directory",
+                    isOpen: true,
+                    children: [
+                        {
+                            name: "Folder",
+                            type: "directory",
+                            isOpen: false,
+                            children: []
+                        },
+                        {
+                            name: "Folder 123",
+                            type: "directory",
+                            isOpen: false,
+                            children: []
+                        },
+                        {
+                            name: "Example File 1",
+                            type: "file",
+                            isOpen: false,
+                        },
+                        {
+                            name: "Example File 2",
+                            type: "file",
+                            isOpen: false,
+                        },
+                        {
+                            name: "Example File 321",
+                            type: "file",
+                            isOpen: false,
+                        },
+                    ]
+                },
+                {
+                    name: "Sample Folder",
+                    type: "directory",
+                    isOpen: false,
+                    children: []
+                },
+                {
+                    name: "Folder 123",
+                    type: "directory",
+                    isOpen: false,
+                    children: []
+                },
+                {
+                    name: "Example File 2",
+                    type: "file",
+                    isOpen: false,
+                },
+                {
+                    name: "Example File 321",
+                    type: "file",
+                    isOpen: false,
+                },
+            ]
+        }
+    ]
     return (
         <main className="qs-main">
             <div className="qs-main__inner">
@@ -66,7 +131,8 @@ export default function Home() {
                 <div className="qs-panel">
                     <div className="qs-panel__head">
                         <div className="qs-panel__head-left">
-                            <Image className="qs-panel__head-icon" src="/images/UploadContractIcon.svg" alt="Project Icon" width={22} height={26}/>
+                            <Image className="qs-panel__head-icon" src="/images/UploadContractIcon.svg"
+                                   alt="Project Icon" width={22} height={26}/>
                             <h1 className="qs-panel__head-title">Sample Project</h1>
                         </div>
                         <div className="qs-panel__head-right">
@@ -76,9 +142,128 @@ export default function Home() {
                     </div>
                     
                     <div className="qs-panel__inner">
-                        <div className="qs-panel__left"></div>
-                        <div className="qs-panel__center"></div>
-                        <div className="qs-panel__right"></div>
+                        <div className="qs-panel__left">
+                            <div className="qs-panel__left-head">
+                                <h2 className="qs-panel__left-title">Folder & Files</h2>
+                            </div>
+                            
+                            <ul className="qs-menu">
+                                {folderFiles.map((item:any,key:number)=> (
+                                    <li className="qs-menu__item" key={key}>
+                                        <div className={`qs-menu__item-inner ${item.isOpen? "open":""}`}>
+                                            {item.type === "directory" ? (
+                                                <>
+                                                    {item.isOpen ?(
+                                                        <>
+                                                            <Icon className="qs-menu__item-arrow" path={mdiMenuDown} size={1} />
+                                                            <Icon className="qs-menu__item-icon" path={mdiFolderOutline} size={1} />
+                                                        </>
+                                                    ):(
+                                                        <>
+                                                            <Icon className="qs-menu__item-arrow" path={mdiMenuRight} size={1} />
+                                                            <Icon className="qs-menu__item-icon" path={mdiFolder} size={1} />
+                                                        </>
+                                                    )}
+                                                
+                                                </>
+                                            ):(
+                                                <Icon className="qs-menu__item-icon" path={mdiFile} size={1} />
+                                            )}
+                                            {item.name}
+                                        </div>
+                                        
+                                        {item.type === "directory" && item.children.length ? (
+                                            <ul className="qs-menu" key={key}>
+                                                {item.children.map((item:any, key:number) => (
+                                                    <li className="qs-menu__item" key={key}>
+                                                        <div className={`qs-menu__item-inner ${item.isOpen? "open":""}`}>
+                                                            {item.type === "directory" ? (
+                                                                <>
+                                                                    {item.isOpen ? (
+                                                                        <>
+                                                                            <Icon className="qs-menu__item-arrow"
+                                                                                  path={mdiMenuDown} size={1}/>
+                                                                            <Icon className="qs-menu__item-icon"
+                                                                                  path={mdiFolderOutline} size={1}/>
+                                                                        </>
+                                                                    ) : (
+                                                                        <>
+                                                                            <Icon className="qs-menu__item-arrow"
+                                                                                  path={mdiMenuRight} size={1}/>
+                                                                            <Icon className="qs-menu__item-icon"
+                                                                                  path={mdiFolder} size={1}/>
+                                                                        </>
+                                                                    )}
+                                                                
+                                                                </>
+                                                            ) : (
+                                                                <Icon className="qs-menu__item-icon" path={mdiFile}
+                                                                      size={1}/>
+                                                            )}
+                                                            {item.name}
+                                                        </div>
+                                                        
+                                                        {item.type === "directory" && item.children.length ? (
+                                                            <ul className="qs-menu" key={key}>
+                                                                {item.children && item.children.map((item:any, key:number) => (
+                                                                    <li className="qs-menu__item" key={key}>
+                                                                        <div className={`qs-menu__item-inner ${item.isOpen? "open":""}`}>
+                                                                            {item.type === "directory" ? (
+                                                                                <>
+                                                                                    {item.isOpen ? (
+                                                                                        <>
+                                                                                            <Icon
+                                                                                                className="qs-menu__item-arrow"
+                                                                                                path={mdiMenuDown}
+                                                                                                size={1}/>
+                                                                                            <Icon
+                                                                                                className="qs-menu__item-icon"
+                                                                                                path={mdiFolderOutline}
+                                                                                                size={1}/>
+                                                                                        </>
+                                                                                    ) : (
+                                                                                        <>
+                                                                                            <Icon
+                                                                                                className="qs-menu__item-arrow"
+                                                                                                path={mdiMenuRight}
+                                                                                                size={1}/>
+                                                                                            <Icon
+                                                                                                className="qs-menu__item-icon"
+                                                                                                path={mdiFolder}
+                                                                                                size={1}/>
+                                                                                        </>
+                                                                                    )}
+                                                                                
+                                                                                </>
+                                                                            ) : (
+                                                                                <Icon className="qs-menu__item-icon"
+                                                                                      path={mdiFile}
+                                                                                      size={1}/>
+                                                                            )}
+                                                                            {item.name}
+                                                                        </div>
+                                                                    </li>
+                                                                ))}
+                                                            </ul>
+                                                        ):""}
+                                                    </li>
+                                                ))}
+                                            </ul>
+                                        ):""}
+                                    </li>
+                                ))}
+                            </ul>
+                        </div>
+                        <div className="qs-panel__center">
+                            <div className="qs-panel__center-head">
+                                <h2 className="qs-panel__center-title">Folder & Files</h2>
+                            </div>
+                        </div>
+                        <div className="qs-panel__right">
+                            <div className="qs-panel__right-head">
+                                <h2 className="qs-panel__right-title">Folder & Files</h2>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
